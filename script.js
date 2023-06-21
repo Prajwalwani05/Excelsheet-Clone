87e7c95a0d3fc5e98245918c1691a9bd47cd93f0
Prajwal Wani <prajwalwani05@gmail.com>
2023-06-21 23:38:43 +0530

    commit

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 let headRow = document.getElementById('head-row');
 let tableBody = document.getElementById('table-body');
 let col = 26;
+let currentCell;
+let cutBtn = document.getElementById('cut');
+let pasteBtn = document.getElementById('paste');
+let copyBtn = document.getElementById('copy');
+let boldBtn = document.getElementById('bold');
+let italicsBtn = document.getElementById('italics');
+let underlineBtn = document.getElementById('underline');
+let lineThrough = document.getElementById('lineThrough');
+let txtColor = document.getElementsByClassName('txtColor')[0];
+let bgColor = document.getElementsByClassName('bgColor')[0];
+let fontFamily = document.getElementById('fontFamily');
+let fontSize = document.getElementById('fontSize');
 
 // running loop to get upper row that containe A-Z chars .
 for(let x=1;x<=col;x++){
@@ -28,9 +40,67 @@ for(let row=1;row<=100;row++){
     }
     tableBody.append(tr);
 }
+// let cut = {
+//     text:currentCell.innerText,
+// };
+// cutBtn.addEventListener('click' , (event)=>{
+//     cut.innerText = currentCell.innerText;
+//     currentCell.innerText='';
+// })
+// pasteBtn.addEventListener('click' ,(event)=>{
+//     currentCell.innerText = cut;
+//     cut = '';
+// })
+boldBtn.addEventListener('click',(event)=>{
+    if(currentCell.style.fontWeight==='bold'){
+        currentCell.style.fontWeight= 'normal';
+    }
+    else
+    currentCell.style.fontWeight='bold';
+});
+
+italicsBtn.addEventListener('click',()=>{
+    if(currentCell.style.fontStyle==='italic')
+    currentCell.style.fontStyle='normal';
+    else
+    currentCell.style.fontStyle='italic';
+})
+
+underlineBtn.addEventListener('click' , ()=>{
+    if(currentCell.style.textDecoration==='underline')
+    currentCell.style.textDecoration='none';
+    else
+    currentCell.style.textDecoration='underline';
+})
+
+lineThrough.addEventListener('click' , ()=>{
+    if(currentCell.style.textDecoration==='line-through')
+    currentCell.style.textDecoration='none';
+    else
+    currentCell.style.textDecoration='line-through';
+})
+
+txtColor.addEventListener('input', ()=>{
+   let textColor = txtColor.value;
+    currentCell.style.color = textColor;
+})
+bgColor.addEventListener('input', ()=>{
+    let textColor = bgColor.value;
+     currentCell.style.backgroundColor = textColor;
+ })
+fontFamily.addEventListener('input' , ()=>{
+    let font = fontFamily.value;
+    currentCell.style.fontFamily = font;
+})
+fontSize.addEventListener('input' , ()=>{
+    let Sfont = fontSize.value;
+    currentCell.style.fontSize = Sfont;
+})
+
+
 let rowColNum = document.getElementById('rowColNum');
 function focus(event){
-   let showId = event.target.id;
-   rowColNum.innerText= showId;
+    currentCell = event.target;
+   rowColNum.innerText= currentCell.id;
 // console.log(event.target)
 }
